cd0610086066da9e16f87e5789ba64d252147a3e
AiraFranco <adelosofranco@gmail.com>
2025-12-05 11:26:49 +0100

    week4_query_sakila

---
+++ lecture_code_along/week4_query_sakila/sql/load_sakila.sql
@@ -0,0 +1,10 @@
+INSTALL sqlite;
+
+LOAD sqlite;
+
+CALL sqlite_attach('data/sqlite-sakila.db'); -- had a problem with the path
+
+-- use the python script in this lecture to run in the interface instead of CLI or terminal
+-- the script is in pandas_duckdb.ipynb
+
+
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from aira-sql!")
+
+
+if __name__ == "__main__":
+    main()
